c36ff3fd94632bfaf9c6038dc9b72a54f2f4596c
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-11-13 15:27:16 +0200

    feat: bubble sort

--- src/main/java/array_utils/Driver.java
+++ src/main/java/array_utils/Driver.java
@@ -1,7 +1,17 @@
 package array_utils;
 
+import java.util.Arrays;
+
 public class Driver {
     public static void main(String[] args) {
+        int[] arr = { 2, 6, 9, 8, 4, 5, 7, 10, 3, 1, 11, 16, 12 };
+        int[] arr2 = { 2, 1, 4, 6, 3, 5 };
+
+        SortUtils.bubbleSort(arr);
+        SortUtils.bubbleSort(arr2);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
 
     }
 }
